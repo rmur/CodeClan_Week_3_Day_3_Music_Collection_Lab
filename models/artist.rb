7cb654b08c5d8f84a0af4d2ba36fa44f0b900523
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-07-12 16:12:14 +0100

    save method for artist created

---
+++ models/artist.rb
@@ -0,0 +1,25 @@
+require('pg')
+require('music_sql_runner')
+
+class Artist
+
+  attr_reader :id
+  attr_accessor :name
+
+  def initialize(options)
+    @name = options['name']
+    @id = options['id'].to_i if options['id']
+  end
+
+  def save
+    sql = "INSERT INTO artists (name) VALUES
+          ('#{@name}') RETURNING id;"
+    @id = SqlRunner.run(sql)[0]['id'].to_i
+  end
+
+
+
+
+
+end
+

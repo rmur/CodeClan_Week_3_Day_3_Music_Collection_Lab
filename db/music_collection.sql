d86058996b2060c751565f86561d7123a92a764b
Chris Donnelly <chris_j_donnelly@hotmail.com>
2017-07-12 15:38:44 +0100

    tables created

---
+++ db/music_collection.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS artists;
+
+CREATE TABLE albums (
+id SERIAL4 PRIMARY KEY,
+title VARCHAR(255),
+genre VARHCHAR(255),
+);
+
+CREATE TABLE artists (
+id SERIAL4 PRIMARY KEY,
+name VARCHAR(255),
+album_id INT4 REFERENCES albums(id)
+);
